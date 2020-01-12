ac3af7f02b33485e9484b77ad870b1882a434d95
rave <ni3fidzialny@gmail.com>
2020-01-12 21:05:21 +0100

    wip

---
+++ src/Components/Pages/Homepage.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {Container, Jumbotron} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+
+class Homepage extends Component {
+  render() {
+    return (
+        <Jumbotron fluid>
+          <Container>
+            <h1>Części</h1>
+            <p>
+              Mamy wszystko do twojego gruchota aby wieś tunning był jeszcze lepszy.
+              Musi być full tunning ~ revolt
+            </p>
+            <p>
+              <Button variant="primary">Przeglądaj ogłoszenia</Button>
+            </p>
+          </Container>
+        </Jumbotron>
+    );
+  }
+}
+
+export default Homepage;
---
+++ src/Components/User/LoginModal.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class LoginModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpened: true
+    }
+  }
+
+  render() {
+    return (
+  <></>
+    );
+  }
+}
+
+export default LoginModal;
